d35a0ef5103b320d3792cf006b47d71ffb43faba
Simisola Karunwi <117954068+Karunwisimi@users.noreply.github.com>
2024-06-27 16:49:16 +0100

    Add files via upload

---
+++ data.py
@@ -0,0 +1,44 @@
+# string = "89"
+# print(type(string))
+# add_string = "78" + "a2" + ' 7 b'
+# print(add_string)
+
+# # numbers
+# # integers and floats
+# number = 89
+# print(type(number))
+
+# no = 89.0
+# print(type(no))
+
+# complex = 2 + 5j
+
+# Adding two data types together
+# add_types = 90 + "gh" 
+
+# # Type casting in python
+# float1= 10000.00
+# print(type(float1))
+
+# # Change float1 to an integer data type. You can change from one numeric data type to another in python.
+# float_int = int(float1)
+# print(type(float_int))
+# print(float_int)
+
+# # You can't turn alphabets or special characters in python to numeric data types (int or float)
+# string = " naira"
+# string_alphabet_int = float(string)
+# # This code will definitely givve an error
+# print(string_alphabet_int)
+
+
+# amount = 500000
+# currency = " Dollars" 
+
+# amount_currency = str(amount)
+# print(amount_currency + str(amount))
+
+# Tuple
+# courses = ("SQL", "Python", "Java", "Excel", "Javascript")
+# print(courses)
+
